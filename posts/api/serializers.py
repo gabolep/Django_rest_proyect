0ece328e6c967af1d6086fa3097347b4beff0d09
root <root@DESKTOP-PV9J7U2.localdomain>
2022-03-30 12:35:41 -0300

    first commit

---
+++ posts/api/serializers.py
@@ -0,0 +1,8 @@
+from pdb import post_mortem
+from rest_framework.serializers import ModelSerializer
+from posts.models import Post
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title','description','order','created_at']
