d47e050d50a8b4af6364996b5e8b3d4ea69891fd
root <root@DESKTOP-PV9J7U2.localdomain>
2022-03-31 11:53:23 -0300

    adding documentation

--- posts/api/views.py
+++ posts/api/views.py
@@ -7,7 +7,7 @@ from posts.api.serializers import PostSerializer
 class PostApiView(APIView):
     def get(self, request):
         serializer = PostSerializer(Post.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)     
     def post(self, request):
         serializer = PostSerializer(data=request.POST)
         serializer.is_valid(raise_exception=True)
